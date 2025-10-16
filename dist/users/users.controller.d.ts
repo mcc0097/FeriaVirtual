c76f0f64036f6e7dc13223d6a4619c36f2c3b215
Jaime <jmj0010@alu.medac.es>
2025-10-16 16:34:49 +0200

    Reorganizado el backend dentro de la carpeta Backend

--- dist/users/users.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { UsersService } from './users.service';
-import { User } from './entities/user.entity';
-export declare class UsersController {
-    private readonly usersService;
-    constructor(usersService: UsersService);
-    findAll(): Promise<User[]>;
-    create(body: Partial<User>): Promise<User>;
-}
--- dist/users/users.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from 'typeorm';
-import { User } from './entities/user.entity';
-export declare class UsersService {
-    private userRepository;
-    constructor(userRepository: Repository<User>);
-    create(userData: Partial<User>): Promise<User>;
-    findAll(): Promise<User[]>;
-}
